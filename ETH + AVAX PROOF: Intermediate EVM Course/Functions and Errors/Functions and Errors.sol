8848767c71e807d90e0b68e593f779c1d769b8c0
sritambiswal2003 <139357786+sritambiswal2003@users.noreply.github.com>
2023-07-27 01:55:10 +0530

    Update Functions and Errors.sol

--- ETH + AVAX PROOF: Intermediate EVM Course/Functions and Errors/Functions and Errors.sol
+++ ETH + AVAX PROOF: Intermediate EVM Course/Functions and Errors/Functions and Errors.sol
@@ -1,1 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract ExceptionHandlingContract {
+    uint public number;
+
+    function setNumber(uint _number) public {
+        number = _number;
+    }
+
+    function requireStatement(uint _number) public view returns (uint) {
+        require(_number > 0, "Number must be greater than zero");
+        return _number;
+    }
+
+    function assertStatement() public view returns (uint) {
+        assert(number == 0);
+        return number;
+    }
+
+    function revertStatement(uint _i) public pure {
+        require(_i > 10, "Input must be greater than 10");
+        
+    }
+}
