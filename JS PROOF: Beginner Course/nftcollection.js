b50d6389bedcf7b289b7f8a2b5e4912a8ed66b40
sritambiswal2003 <139357786+sritambiswal2003@users.noreply.github.com>
2023-07-31 11:01:08 +0530

    Update nftcollection.js

--- JS PROOF: Beginner Course/nftcollection.js
+++ JS PROOF: Beginner Course/nftcollection.js
@@ -1,1 +1,39 @@
+const vault = [];
 
+function mintNFT(name, model, year, category) {
+  const nft = {
+    name: name,
+    model: model,
+    year: year,
+    category: category,
+  };
+  vault.push(nft);
+}
+
+function listNFTs() {
+  console.log("The Generated NFTs are : ");
+  for (const nft of vault) {
+    console.log(
+      `[${vault.indexOf(nft) + 1}].`,
+      nft.name,
+      nft.model,
+      nft.year,
+      nft.category
+    );
+  }
+}
+
+function getTotalSupply() {
+  return vault.length;
+}
+
+
+
+mintNFT("Ford ", "GT-40", 1964, "Race car");
+mintNFT("MClaren", "MP4/4", 1984, "Race Car");
+mintNFT("Bentley", "Flying spur", 2023, "Sedan");
+mintNFT("Maserati", "quattroporte", 2019, "Sports sedan");
+
+
+listNFTs();
+console.log(getTotalSupply());
