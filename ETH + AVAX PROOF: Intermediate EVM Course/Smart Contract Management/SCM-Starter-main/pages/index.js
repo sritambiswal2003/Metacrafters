d9e328ea09a7864f9d10f7cf4981f348abd6e6b5
sritambiswal2003 <139357786+sritambiswal2003@users.noreply.github.com>
2023-08-05 10:51:19 +0530

    Update index.js

--- ETH + AVAX PROOF: Intermediate EVM Course/Smart Contract Management/SCM-Starter-main/pages/index.js
+++ ETH + AVAX PROOF: Intermediate EVM Course/Smart Contract Management/SCM-Starter-main/pages/index.js
@@ -1,210 +1,127 @@
-import React, { useState, useEffect } from "react";
-import { ethers } from "ethers";
-import atm_abi from "ETH + AVAX PROOF: Intermediate EVM Course/Smart Contract Management/SCM-Starter-main/contracts/Assessment.sol";
-import backgroundImage from "ETH + AVAX PROOF: Intermediate EVM Course/Smart Contract Management/SCM-Starter-main/item/images.jpg";
+window.addEventListener('load', async () => {
+  // Check if Web3 is injected by the browser (modern browsers with MetaMask)
+  if (typeof web3 !== 'undefined') {
+    // Use the existing provider
+    window.web3 = new Web3(web3.currentProvider);
+  } else {
+    // Set up a new provider (e.g., local development blockchain)
+    window.web3 = new Web3('http://localhost:8545');
+  }
 
-export default function HomePage() {
-  const [ethWallet, setEthWallet] = useState(null);
-  const [account, setAccount] = useState(null);
-  const [atm, setATM] = useState(null);
-  const [balance, setBalance] = useState(0);
-
-  const [withdrawAmount, setWithdrawAmount] = useState("");
-  const [buyItem, setBuyItem] = useState("");
-
-  const contractAddress = "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4";
-  const atmABI = atm_abi.abi;
-
-  const getWallet = async () => {
-    if (window.ethereum) {
-      setEthWallet(new ethers.providers.Web3Provider(window.ethereum));
+  // Contract address and ABI (Replace with your deployed contract details)
+  const contractAddress = 'CONTRACT_ADDRESS';
+  const contractABI = [
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_id",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "_name",
+          "type": "string"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_age",
+          "type": "uint256"
+        }
+      ],
+      "name": "addStudent",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_id",
+          "type": "uint256"
+        }
+      ],
+      "name": "getStudent",
+      "outputs": [
+        {
+          "components": [
+            {
+              "internalType": "uint256",
+              "name": "id",
+              "type": "uint256"
+            },
+            {
+              "internalType": "string",
+              "name": "name",
+              "type": "string"
+            },
+            {
+              "internalType": "uint256",
+              "name": "age",
+              "type": "uint256"
+            }
+          ],
+          "internalType": "struct SchoolManagement.Student",
+          "name": "",
+          "type": "tuple"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getTotalStudents",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
     }
-  };
+  ];
 
-  const handleAccount = (account) => {
-    if (account && account.length > 0) {
-      console.log("Account connected:", account[0]);
-      setAccount(account[0]);
-    } else {
-      console.log("No account found");
-    }
-  };
+  // Create contract instance
+  const schoolContract = new web3.eth.Contract(contractABI, contractAddress);
 
-  const connectAccount = async () => {
-    if (!ethWallet) {
-      alert("MetaMask wallet is required to connect");
-      return;
-    }
-
+  // Function to add a student
+  const addStudent = async () => {
+    const id = document.getElementById('id').value;
+    const name = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
     try {
-      const accounts = await ethWallet.request({ method: "eth_requestAccounts" });
-      handleAccount(accounts);
-      getATMContract();
-    } catch (error) {
-      console.error("Error connecting to the account:", error);
+      await schoolContract.methods.addStudent(id, name, age).send({ from: web3.eth.defaultAccount });
+      document.getElementById('output').innerHTML = `Student added: ID - ${id}, Name - ${name}, Age - ${age}`;
+    } catch (err) {
+      console.error(err);
+      document.getElementById('output').innerHTML = 'Error adding student.';
     }
   };
 
-  const getATMContract = () => {
-    if (ethWallet) {
-      const signer = ethWallet.getSigner();
-      const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
-      setATM(atmContract);
-    }
-  };
-
-  const getBalance = async () => {
+  // Function to get a student
+  const getStudent = async () => {
+    const id = document.getElementById('id').value;
     try {
-      if (atm) {
-        const balance = await atm.getBalance();
-        setBalance(balance.toNumber());
-      }
-    } catch (error) {
-      console.error("Error fetching balance:", error);
+      const student = await schoolContract.methods.getStudent(id).call();
+      document.getElementById('output').innerHTML = `Student found: ID - ${student[0]}, Name - ${student[1]}, Age - ${student[2]}`;
+    } catch (err) {
+      console.error(err);
+      document.getElementById('output').innerHTML = 'Student not found.';
     }
   };
 
-  const deposit = async () => {
-    if (atm && depositAmount) {
-      try {
-        const tx = await atm.deposit(depositAmount);
-        await tx.wait();
-        getBalance();
-        setDepositAmount("");
-      } catch (error) {
-        console.error("Error depositing ETH:", error);
-      }
+  // Function to get total students
+  const getTotalStudents = async () => {
+    try {
+      const totalStudents = await schoolContract.methods.getTotalStudents().call();
+      document.getElementById('output').innerHTML = `Total Students: ${totalStudents}`;
+    } catch (err) {
+      console.error(err);
+      document.getElementById('output').innerHTML = 'Error getting total students.';
     }
   };
-
-  const withdraw = async () => {
-    if (atm && withdrawAmount) {
-      try {
-        const tx = await atm.withdraw(withdrawAmount);
-        await tx.wait();
-        getBalance();
-        setWithdrawAmount("");
-      } catch (error) {
-        console.error("Error withdrawing ETH:", error);
-      }
-    }
-  };
-
-  const buy = async () => {
-    if (atm && buyItem) {
-      try {
-        const tx = await atm.buy(buyItem);
-        await tx.wait();
-        getBalance();
-        setBuyItem("");
-      } catch (error) {
-        console.error("Error buying item:", error);
-      }
-    }
-  };
-
-  const [showBalance, setShowBalance] = useState(false);
-  const [showAccount, setShowAccount] = useState(false);
-  const [depositAmount, setDepositAmount] = useState("");
-
-  useEffect(() => {
-    getWallet();
-  }, []);
-
-  const initUser = () => {
-    // Check if user has Metamask
-    if (!ethWallet) {
-      return <p>Please install Metamask in order to use this ATM.</p>;
-    }
-
-    // Check if user is connected. If not, connect to their account
-    if (!account) {
-      return (
-        <button onClick={connectAccount}>
-          Please connect your Metamask wallet
-        </button>
-      );
-    }
-
-    return (
-      <div className="atm-container">
-        <div>
-          <button
-            style={{ width: "200px", marginBottom: "10px", fontSize: "25px" }}
-            onClick={() => setShowAccount(!showAccount)}
-          >
-            {showAccount ? "Hide Account" : "Show Account"}
-          </button>
-          {showAccount && <p>Your Account: {account}</p>}
-        </div>
-        <div>
-          <button
-            style={{ width: "200px", marginBottom: "10px", fontSize: "25px" }}
-            onClick={() => {
-              setShowBalance(!showBalance);
-              if (!showBalance) {
-                getBalance();
-              }
-            }}
-          >
-            {showBalance ? "Hide Balance" : "Show Balance"}
-          </button>
-          {showBalance && <p>Your Balance: {balance}</p>}
-        </div>
-        <div>
-          <button
-            style={{ width: "200px", marginBottom: "10px", fontSize: "25px" }}
-            onClick={deposit}
-          >
-            Deposit ETH
-          </button>
-          <input
-            type="number"
-            value={depositAmount}
-            onChange={(e) => setDepositAmount(e.target.value)}
-            placeholder="Enter deposit amount"
-            style={{ width: "180px" }}
-          />
-        </div>
-        <div>
-          <button
-            style={{ width: "200px", marginBottom: "10px", fontSize: "25px" }}
-            onClick={withdraw}
-          >
-            Withdraw ETH
-          </button>
-          <input
-            type="number"
-            value={withdrawAmount}
-            onChange={(e) => setWithdrawAmount(e.target.value)}
-            placeholder="Enter withdrawal amount"
-            style={{ width: "180px" }}
-          />
-        </div>
-        <div>
-          <table style={{ margin: "20px auto", borderCollapse: "collapse" }}>
-            <thead>
-              <tr>
-                <th style={{ border: "3px solid black", padding: "8px" }}>
-                  Item No.
-                </th>
-                <th style={{ border: "3px solid black", padding: "8px" }}>
-                  NFT Name
-                </th>
-                <th style={{ border: "3px solid black", padding: "8px" }}>
-                  Price (in ETH)
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td style={{ border: "3px solid black", padding: "8px" }}>1</td>
-                <td style={{ border: "3px solid black", padding: "8px" }}>
-                  EtherGlow
-                </td>
-                <td style={{ border: "3px solid black", padding: "8px" }}>
-                  150
-                </td>
-              </tr>
-              <tr>
-                <td style={{ border
+});
