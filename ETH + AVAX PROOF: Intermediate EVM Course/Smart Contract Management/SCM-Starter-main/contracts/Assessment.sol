ab4243c78d197c08d78e7851e9899b098fd83296
sritambiswal2003 <139357786+sritambiswal2003@users.noreply.github.com>
2023-08-05 19:13:47 +0530

    Update Assessment.sol

--- ETH + AVAX PROOF: Intermediate EVM Course/Smart Contract Management/SCM-Starter-main/contracts/Assessment.sol
+++ ETH + AVAX PROOF: Intermediate EVM Course/Smart Contract Management/SCM-Starter-main/contracts/Assessment.sol
@@ -1,26 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SchoolManagement {
-    struct Student {
-        uint256 id;
-        string name;
-        uint256 age;
+contract NewBank {
+    mapping(address => string) accountData;
+
+    event FundsTransferred(uint256 amount);
+    event DataUpdated(string value);
+
+    function setAccountData(string memory _data) public {
+        require(
+            keccak256(abi.encodePacked(accountData[msg.sender])) !=
+                keccak256(abi.encodePacked(_data)),
+            "New data is same as old data"
+        );
+        accountData[msg.sender] = _data;
+
+        emit DataUpdated(accountData[msg.sender]);
     }
 
-    mapping(uint256 => Student) private students;
-    uint256 private totalStudents;
-
-    function addStudent(uint256 _id, string memory _name, uint256 _age) external {
-        students[_id] = Student(_id, _name, _age);
-        totalStudents++;
+    function getAccountData() public view returns (string memory) {
+        return accountData[msg.sender];
     }
 
-    function getStudent(uint256 _id) external view returns (uint256, string memory, uint256) {
-        return (students[_id].id, students[_id].name, students[_id].age);
+    error InsufficientBalanceError(uint256 balance, uint256 withdrawAmount);
+
+    function transferEther(address payable _to) public payable {
+        _to.transfer(msg.value);
+        if (msg.sender.balance < msg.value)
+            revert InsufficientBalanceError({
+                balance: msg.sender.balance,
+                withdrawAmount: msg.value
+            });
+        emit FundsTransferred(msg.value);
     }
 
-    function getTotalStudents() external view returns (uint256) {
-        return totalStudents;
+    function getAccountBalance() public view returns (uint256) {
+        return msg.sender.balance;
     }
 }
