64a2990a4182c7fee25a1ee7475a1aa6846f2476
sritambiswal2003 <139357786+sritambiswal2003@users.noreply.github.com>
2023-08-06 12:12:56 +0530

    Update deploy.js

--- ETH + AVAX PROOF: Intermediate EVM Course/Smart Contract Management/SCM-Starter-main/scripts/deploy.js
+++ ETH + AVAX PROOF: Intermediate EVM Course/Smart Contract Management/SCM-Starter-main/scripts/deploy.js
@@ -7,12 +7,12 @@
 const hre = require("hardhat");
 
 async function main() {
+  const initBalance = 1;
+  const Assessment = await hre.ethers.getContractFactory("Assessment");
+  const assessment = await Assessment.deploy(initBalance);
+  await assessment.deployed();
 
-  const bank = await hre.ethers.deployContract("Bank");
-
-  await bank.waitForDeployment();
-
-  console.log("Bank deployed to:", await bank.getAddress());
+  console.log(`A contract with balance of ${initBalance} eth deployed to ${assessment.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
