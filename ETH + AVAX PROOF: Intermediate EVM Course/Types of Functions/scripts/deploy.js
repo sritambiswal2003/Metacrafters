97abae6aeb8fd8c468739558f298fb45d7cc959f
sritambiswal2003 <139357786+sritambiswal2003@users.noreply.github.com>
2023-08-25 13:00:41 +0530

    Update deploy.js

--- ETH + AVAX PROOF: Intermediate EVM Course/Types of Functions/scripts/deploy.js
+++ ETH + AVAX PROOF: Intermediate EVM Course/Types of Functions/scripts/deploy.js
@@ -1,16 +1,20 @@
 // scripts/deploy.js
-async function main () {
-  // We get the contract to deploy
-  const Token = await ethers.getContractFactory('Token');
-  console.log('Deploying Token...');
-  const token = await Token.deploy();
-  await token.deployed();
-  console.log('Token deployed to:', token.address);
-}
-
-main()
-  .then(() => process.exit(0))
-  .catch(error => {
+async function main() {
+  try {
+    // We get the contract to deploy
+    const Token = await ethers.getContractFactory('Token');
+    console.log('Deploying Token...');
+    
+    const token = await Token.deploy();
+    await token.deployed();
+    
+    console.log('Token deployed to:', token.address);
+    
+    process.exit(0);
+  } catch (error) {
     console.error(error);
     process.exit(1);
-  });
+  }
+}
+
+main();
